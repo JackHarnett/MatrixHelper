f59755348617ff0f57891414194755b0f2643e77
Jack <jackery4444@hotmail.co.uk>
2019-02-22 14:52:42 +0000

    Removed accidental changes.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,4 @@
 #include "vec3.h"
 #include "matrix.h"
 
-int main() {
-    auto vec1 = num_vec<float>(3, new float[3] {1, 2, 3});
-    auto vec2 = num_vec<float>(3, new float[3] {4, 5, 6});
-    auto vec3 = num_vec<float>(3, new float[3] {7, 8, 9});
-
-    auto mat1 = matrix<float>(3, new num_vec<float>[3] {vec1, vec2, vec3});
-    mat1.print();
-}
+int main() {}
